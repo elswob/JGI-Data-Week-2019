99ccc1e3c0b48d52a9ba5b5b36d017ce42a3841e
Ben Elsworth <be15516@bristol.ac.uk>
2019-05-14 16:36:33 +0100

    tidying up

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
 #output
 orcidFile='output/orcid.tsv'
 pubmedFile='output/pubmed.tsv'
-
-#pure
-purePeople=''
+tfidfFile='output/orcid-tf-idf.txt'
+orcidToOrcid='output/orcid-to-orcid-tf-idf.tsv'
 
 #data
+demoPubmedFile='data/pubmed.tsv'
+demoOrcidFile='data/orcid.tsv'
+demoTfidfFile='data/orcid-tf-idf.txt'
+demoPurePeopleFile='data/pure_people.txt'
+demoPureOrcidFile='data/pure_person_to_orcid.txt'
+demoOrcidToOrcid='output/orcid-to-orcid-tf-idf.tsv'
+
+
 demo1='data/demo.tsv'
-demoPubmed='data/pubmed.tsv'
 demo2='data/pure_person_to_orcid.txt'
